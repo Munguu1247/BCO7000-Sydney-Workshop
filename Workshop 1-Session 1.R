c2a18a50ae0413a33e8c85b8c7b5add1b60d7ff7
Mungunchimeg Batbayar <mungunchimeg.batbayar@live.vu.edu.au>
2022-02-24 11:04:02 +1100

    Mungunchimeg.B

---
+++ Workshop 1-Session 1.R
@@ -0,0 +1,25 @@
+#150/3
+
+a <- 10
+
+b <- "Cool"
+
+c <-c(3,4,7,9)
+
+summary(c)
+
+summary(c(3,4,7,9))
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+rm(b)
+
+data(mtcars)
+
+head(mtcars)
+
+summary(mtcars)
+
+glimpse(mtcars)
